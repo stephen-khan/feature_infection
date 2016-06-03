4e61ec1a56e64927d5832c25e1d824ce8b66b27e
Stephen Rauschmayer <stephen.rauschmayer@gmail.com>
2016-06-03 00:40:39 -0400

    Created initial implementation of total_infection using the networkx library

--- infection.py
+++ infection.py
@@ -4,10 +4,25 @@ Infection
 
 Control release of changes to coaches and students
 """
+import networkx as nx
 
-def total_infection():
+class User(object):
+    """Simplified user representation"""
+    def __init__(self):
+        self.connections = []
+
+def total_infection(users, target_user):
     """Create an infection of all users connected to the target user"""
-    pass
+    if not target_user:
+        return []
+
+    user_graph = nx.Graph()
+    for user in users:
+        user_graph.add_node(user)
+        for coached_student in user.connections:
+            user_graph.add_edge(user, coached_student)
+    connected_users = nx.node_connected_component(user_graph, target_user)
+    return list(connected_users)
 
 def limited_infection():
     """Create an infection that is bounded by the target size"""
--- test_infection.py
+++ test_infection.py
@@ -1,8 +1,33 @@
 import infection
+User = infection.User
 
-def test_total_infection():
-    infection.total_infection()
-    assert True
+def test_total_infection_of_no_users():
+    users = []
+    assert infection.total_infection(users, None) == []
+
+def test_total_infection_of_single_user():
+    users = [User()]
+    assert infection.total_infection(users, users[0]) == users
+
+def test_total_infection_of_disconnected_user():
+    users = [User(), User()]
+    assert infection.total_infection(users, users[0]) == users[0:1]
+
+def test_total_infection_of_connected_users():
+    user1 = User()
+    user2 = User()
+    user1.connections.append(user2)
+    users = [user1, user2]
+    assert sorted(infection.total_infection(users, user1)) == sorted(users)
+    assert sorted(infection.total_infection(users, user2)) == sorted(users)
+
+def test_total_infection_of_disjoint_connected_users():
+    users = [User(), User(), User(), User()]
+    users[0].connections.append(users[1])
+    users[2].connections.append(users[3])
+    assert sorted(infection.total_infection(users, users[0])) == sorted(users[:2])
+    assert sorted(infection.total_infection(users, users[2])) == sorted(users[2:])
+    
 
 def test_limited_infection():
     infection.limited_infection()
