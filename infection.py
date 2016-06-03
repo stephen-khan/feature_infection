443adff8442adc737f462d886083778ce8b205a0
Stephen Rauschmayer <stephen.rauschmayer@gmail.com>
2016-06-02 22:46:46 -0400

    Add pylint early in the project and ensure that our current stub code is clean

--- infection.py
+++ infection.py
@@ -1,5 +1,14 @@
+"""
+Khan Academy coding project
+Infection
+
+Control release of changes to coaches and students
+"""
+
 def total_infection():
+    """Create an infection of all users connected to the target user"""
     pass
 
 def limited_infection():
+    """Create an infection that is bounded by the target size"""
     pass
