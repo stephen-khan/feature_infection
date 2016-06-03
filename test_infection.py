236970eb80fffa20e56365b1f50c72a500b5838e
Stephen Rauschmayer <stephen.rauschmayer@gmail.com>
2016-06-02 23:02:34 -0400

    Create unit test stub for the project.

---
+++ test_infection.py
@@ -0,0 +1,9 @@
+import infection
+
+def test_total_infection():
+    infection.total_infection()
+    assert True
+
+def test_limited_infection():
+    infection.limited_infection()
+    assert True
