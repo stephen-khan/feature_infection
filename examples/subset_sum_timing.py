53fc4ca574a81d79d610b5531bf8ac4fb6107b28
Stephen Rauschmayer <stephen.rauschmayer@gmail.com>
2016-06-07 08:26:20 -0400

    Updated the subset sum timing to use the same graph for each algorithm at a given size to improve the comparison.  Also collected relative error.

--- examples/subset_sum_timing.py
+++ examples/subset_sum_timing.py
@@ -1,56 +1,84 @@
 import random
 import time
 from functools import partial
-from itertools import takewhile
+from itertools import groupby, imap
+from operator import itemgetter
+from collections import namedtuple, defaultdict
 #from multiprocessing import Pool
 
-from infection import subset_sum
+from feature_infection import subset_sum
 
-def takeuntil(predicate, iterable):
-    # takewhile(lambda x: x<5, [1,4,6,4,1]) --> 1 4
-    for x in iterable:
-        yield x
-        if not predicate(x):
-            break
+# An implementation of execut solution to subset sum optimization that returns
+# only the sum, not the subset itself.  The real version runs out of memory
+# between 30 and 100 elements.
+def unique_justseen(iterable, key=None):
+    "List unique elements, preserving order. Remember only the element just seen."
+    return map(next, imap(itemgetter(1), groupby(iterable, key)))
 
-def trial(trial_num, algo, sample_size, element_size, proportion):
-    sample = [random.randint(0,element_size) for r in xrange(sample_size)]
+def extend(L, x):
+    return map(lambda l: l + x, L)
+
+def merge_list(U, V):
+    return unique_justseen(sorted(U+V))
+
+def exact(S, t):
+    n = len(S)
+    L = [0]
+    for i in xrange(n):
+        #print L, ",", S[i], "->",
+        L = merge_list(L, extend(L, S[i]))
+        L = filter(lambda l: l <= t, L)
+        #print L
+    return max(L)
+
+
+TestData = namedtuple("TestData", ["samples", "target"])
+def create_test_data(sample_size, element_size, proportion):
+    sample = [random.randint(1,element_size) for r in xrange(sample_size)]
     target = int(sum(sample) * proportion)
+    return TestData(sample, target)
+
+def trial(algo, test_data):
     start = time.clock()
-    res = subset_sum.optimize(sample, target, algo=algo)
-    return time.clock() - start
+    if algo != "exact":
+        res, _ = subset_sum.optimize(test_data.samples, test_data.target, algo=algo)
+    else:
+        res = exact(test_data.samples, test_data.target)
+    return time.clock() - start, res
 
-def repeat_trial(num_trials, algo, sample_size, element_size, proportion):
-    partial_trial = partial(trial, algo=algo, sample_size=sample_size,
-                             element_size=element_size, proportion=proportion)
-    #pool = Pool(8)
-    runnings = map(partial_trial, xrange(num_trials))
-    return sum(runnings) / num_trials
-
-def trial_sizes(algo, trials, element_size, proportion):
+def get_report_data():
+    active_algos = set(subset_sum.ALGORITHMS)
     sample_size = 100
-    while True:
-        #print "running", algo, sample_size
-        yield repeat_trial(trials, algo, sample_size, element_size, proportion)
+    overtime = 10
+    metadata = defaultdict(list)
+    timings = defaultdict(list)
+    results = defaultdict(list)
+
+    while active_algos:
+        metadata["sample_size"].append(sample_size)
+        test_data = create_test_data(sample_size, 10, .2)
+        metadata["target"].append(test_data.target)
+        algos_overtime = set()
+        for algo in active_algos:
+            timing, result = trial(algo, test_data)
+            timings[algo].append(timing)
+            results[algo].append(result)
+            if timing > overtime:
+                algos_overtime.add(algo)
         sample_size *= 2
+        active_algos ^= algos_overtime
+    return metadata, timings, results
 
-def execution_limit(duration):
-    return lambda running: running <= duration
 
-def test_algo(algo, element_size, proportion):
-    num_trials = 1
-    trials = trial_sizes(algo, num_trials, element_size, proportion)
-    return list(takeuntil(execution_limit(1), trials))
-
-def test_all_algos(element_size, proportion):
-    algorithms = subset_sum.algorithms
-    algorithms.remove("exact")
-    for algo in algorithms:
-        yield [algo] + map(str,test_algo(algo, element_size, proportion))
-
-def create_report(element_size, proportion):
-    for result in test_all_algos(element_size, proportion):
-        print ", ".join(result)
+def create_report(metadata, timings, results):
+    print ", ".join(["sample_size"] + map(str, metadata['sample_size']))
+    for algo in subset_sum.ALGORITHMS:
+        print ", ".join([algo] + map(str, timings[algo]))
+    print
+    print ", ".join(["target"] + map(str, metadata['target']))
+    for algo in subset_sum.ALGORITHMS:
+        print ", ".join([algo] + map(str, results[algo]))
 
 if __name__ == "__main__":
-    create_report(10, .2)
+    report_data = get_report_data()
+    create_report(*report_data)
